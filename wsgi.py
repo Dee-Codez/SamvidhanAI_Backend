94aa95d58263e515d947ad78e01ac8b1a9dfbc1c
Dee-Codez <debampati44@gmail.com>
2023-10-31 19:14:36 +0530

    Reverse Fix

--- wsgi.py
+++ wsgi.py
@@ -2,38 +2,31 @@ from flask import Flask,jsonify,request
 from flask_cors import CORS
 import os
 from dotenv import load_dotenv
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
 
 load_dotenv()
 API_KEY = os.getenv('chatnbx_api_key')
 
 
-app = Flask(__name__,static_url_path='/')
+app = Flask(__name__,static_folder='./dist',static_url_path='/')
 CORS(app)
 
-@app.route('/', methods=['POST','GET'])
-def api():
-    if request.method == 'POST':
-        return predict(request)
-    else:
-        return hello()
-
-
-def predict(request):
+@app.route('/api', methods=['POST'])
+def predict():
     data = request.get_json()
     prompt = data['prompt']
-
+    from langchain.llms import OpenAI
+    from langchain.chat_models import ChatOpenAI
     chat_model = ChatOpenAI(
         openai_api_key=API_KEY,
         openai_api_base="https://chat.nbox.ai/api/",
-        model_name="llama-2-chat-13b-4k"
+        model_name="llama2-chat-13b-4k"
     )
     out = chat_model.predict(prompt)
     return jsonify({"answer": out})
     
+@app.route('/', methods=['GET'])
 def hello():
     return jsonify({"answer":"This is a Law Based Chatbot"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", threaded=True, port=10000)
+    app.run(host="0.0.0.0", threaded=True, port=5000)
